7fc73957d49240f57e7f25233bc52942038abfa5
maciej <m.sobiech.wat@gmail.com>
2022-10-01 17:26:16 +0200

    yes

--- black_hat/sniffing/sniffer.py
+++ black_hat/sniffing/sniffer.py
@@ -1,15 +1,18 @@
+"""Program decode the IP header from packet without option field"""
 from ctypes import Structure, c_ubyte, c_uint, c_ushort
 import socket
 import struct
 import os
 import sys
-import ipaddress
 
-"""
+
+"""Create the structure of IP packet
 c_ubyte unsigned char
 c_ushort unsigned  short
 c_ubyte unsigned intetger
 """
+
+OS_NAME = "nt"  # nt for windows
 
 
 class IP(Structure):
@@ -30,8 +33,9 @@ class IP(Structure):
         return cls.from_buffer_copy(socket_buffer)
 
     def __init__(self, socket_buffer=None):
-        self.src_address = socket.inet_ntoa(struct.pack("L", self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("L", self.dst))
+        # convert to readeable option
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
         self.protocal_map = {1: "ICMP", 6:"TCP", 17: "UDP"}
         try:
             self.protocol = self.protocal_map[self.protocol_num]
@@ -39,39 +43,36 @@ class IP(Structure):
             print("%s \nNO protocl for %s") % (e, self.protocl_num)
             self.protocol = str(self.protocol_num)
 
+class ICMP(Structure):
+    pass
+
 
 def sniff(host):
-    if os.name == "nt":
+    if os.name == OS_NAME:
         socket_protocol = socket.IPPROTO_IP
     else:
         socket_protocol = socket.IPPROTO_ICMP
-
-    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol) # raw socket
     sniffer.bind((host, 0))
-    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)  # include IP headers
 
-    if os.name == "nt":
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    if os.name == OS_NAME:
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON) # promisous mode for windows
 
     try:
         while True:
-            raw_buffer = sniffer.recvfrom(65535)[0]
-            ip_header = IP(raw_buffer)[0:20]
-            print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+            raw_buffer = sniffer.recvfrom(65535)[0] # catch first packet
+            ip_header = IP(raw_buffer)[0:20] # create IP header object
+            print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)) # pull out protocol src and dest from packet
     
     except KeyboardInterrupt:
-        if os.name == "nt":
-            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        if os.name == OS_NAME:
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF) # trun of prmiscous mode
         sys.exit()
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         host = sys.argv[1]
     else:
-        host = "192.168.8.100"
+        host = "192.168.1.173"
     sniff(host)
-
-
-
-
-
